e6d9013359e7420ae0b1a184314bcdbdca9dfc26
MrMirotic <14301166@bjtu.edu.cn>
2017-05-04 01:49:04 +0800

    Add files via upload

--- php/getDataset.php
+++ php/getDataset.php
@@ -4,11 +4,11 @@
   //$redis->lpush("datasetid","001");
   //$redis->hMset('dataset001',array('id'=>'001','name'=>'secondDataset','desc'=>'the second dataset'));
   $ids = $redis->lrange("datasetid",0,-1);
-  $i = 0;
+  $num = 0;
   foreach ($ids as $id)
   {
-    $datasets[$i] = $redis->hGetAll('dataset'.$id);
-    $i++;
+      $datasets[$num] = $redis->hGetAll('dataset'.$id);
+      $num++;
   }
   $ans['success'] = '0';
   $ans['data'] = $datasets;
---
+++ php/pushData.php
@@ -0,0 +1,39 @@
+<?php
+  function randId()
+  {
+    $a = "";
+    for($i=0;$i<11;$i++)
+    {
+      $a = $a.rand(0,9);
+    }
+    return $a;
+  }
+
+  $redis = new Redis();
+  $redis->connect('127.0.0.1',6379);
+
+  $jsonData = $_REQUEST["jsonData"];
+
+
+  $obj = json_decode($jsonData);
+  print_r($obj);
+  $datasetId = $obj->datasetId;
+
+  $data = $obj->data;
+  for($i=0;$i<count($data);$i++)
+  {
+    $dataId = randId();
+    $redis->rpush('dataid'.$datasetId,$dataId);
+    foreach ($data[$i] as $key => $value)
+    {
+      $redis->hMset('data'.$dataId,array($key=>$value));
+    }
+  }
+
+  $ans['success'] = 0;
+  $ans['errorInfo'] = '';
+  echo json_encode($ans,JSON_UNESCAPED_UNICODE);
+
+
+
+?>
